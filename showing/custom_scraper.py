6c513c6f70c5d0251d6ecb719fba5f6f362e3205
Moiz Irshad <mirza.moiz65@gmail.com>
2024-06-22 03:44:19 +0500

    fix: add arguments for ubuntu server

--- showing/custom_scraper.py
+++ showing/custom_scraper.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 def chrome_scrape():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     # Set up Selenium WebDriver
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
