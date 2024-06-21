15b9e485d337ff53b91bc63ad7cd232ce769ea82
Moiz Irshad <mirza.moiz65@gmail.com>
2024-06-22 03:17:59 +0500

    fix: set static url

--- coins/settings.py
+++ coins/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
